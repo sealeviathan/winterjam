a4c6e2fd1935958f812fa7f52f1311ced6953eec
sealeviathan <63617690+sealeviathan@users.noreply.github.com>
2020-12-16 11:35:04 -0700

    Added grenade and Grenade.cs

---
+++ Overt Ops/Assets/Scripts/Throwables/Grenade.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    public float fuseTime = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fuseTime-=Time.deltaTime;
+        if(fuseTime < 0)
+        {
+            Debug.Log("BOOM");
+            Explode();
+        }
+    }
+    void Explode()
+    {
+        
+        Destroy(gameObject);
+    }
+}
