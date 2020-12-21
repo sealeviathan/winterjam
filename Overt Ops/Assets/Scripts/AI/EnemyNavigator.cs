359bbeadb1b1a6e76f28773dcef6fff988a2a2b4
sealeviathan <63617690+sealeviathan@users.noreply.github.com>
2020-12-20 22:50:41 -0700

    some fixes, and tested a bit of navmesh with a tracking cylinder.

---
+++ Overt Ops/Assets/Scripts/AI/EnemyNavigator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyNavigator : MonoBehaviour
+{
+    public Transform goal;
+    UnityEngine.AI.NavMeshAgent agent;
+       
+    void Start ()
+    {
+       agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+       
+    }
+    private void Update()
+    {
+        agent.destination = goal.position;
+       
+    }
+}
